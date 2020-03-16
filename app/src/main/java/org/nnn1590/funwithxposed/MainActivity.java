4009b53386d16c66e94361299b155a910d36fc04
nnn1590 <28985763+nnn1590@users.noreply.github.com>
2020-03-16 18:33:28 +0900

    Initial commit

---
+++ app/src/main/java/org/nnn1590/funwithxposed/MainActivity.java
@@ -0,0 +1,13 @@
+package org.nnn1590.funwithxposed;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
